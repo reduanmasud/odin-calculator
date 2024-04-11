a1522a9e711b4bbdc27cea7d1f5c1fb8a7a4f5d7
Reduan Masud <reduanmasudcse@gmail.com>
2024-04-11 19:02:54 +0600

    id attribute added to every button to select easily

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const CALCULATE = {
     div: (a, b) => (b == 0 ? "ERROE" : a / b),
 };
 
-console.log(CALCULATE.sub(4, 2));
-console.log(CALCULATE.sum(4, 4));
-console.log(CALCULATE.prod(3, 2));
-console.log(CALCULATE.div(4,2));
+let operand1;
+let operand2;
+let operator;
+let result;
+
+
