8e152bf8ec8ba6338ca63c128152970273271a25
Reduan Masud <reduanmasudcse@gmail.com>
2024-04-11 19:24:38 +0600

    All button added to js

--- script.js
+++ script.js
@@ -8,4 +8,27 @@ const CALCULATE = {
 let operand;
 let operator;
 
+const BTN = {
+    zero : () => document.querySelector('#zero'),
+    one : () => document.querySelector('#one'),
+    two : () => document.querySelector('#two'),
+    three : () => document.querySelector('#three'),
+    four : () => document.querySelector('#four'),
+    five : () => document.querySelector('#five'),
+    six : () => document.querySelector('#six'),
+    seven : () => document.querySelector('#seven'),
+    eight : () => document.querySelector('#eight'),
+    nine : () => document.querySelector('#nine'),
+    dot : () => document.querySelector('#dot'),
+    back : () => document.querySelector('#back'),
+    plus : () => document.querySelector('#plus'),
+    minus : () => document.querySelector('#minus'),
+    div : () => document.querySelector('#div'),
+    multiply : () => document.querySelector('#multiply'),
+    ac : () => document.querySelector('#ac'),
+    plus_minus : () => document.querySelector('#plus_minus'),
+    equal : () => document.querySelector('#equal'),
+    percent : () => document.querySelector('#percent')
+}
+
 
